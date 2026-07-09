a1e100be8b679142f42f3de121338c5dcc818da8
Monikaselvamgit <166580400+Monikaselvamgit@users.noreply.github.com>
2026-07-09 09:44:50 +0530

    Create 144-BinaryTreePreorderTraversal

---
+++ 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.java
@@ -0,0 +1,29 @@
+// Last updated: 7/9/2026, 9:44:49 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+         List<Integer> res=new LinkedList<>();
+         traverse(root,res);
+         return res;
+    }
+    void traverse(TreeNode node, List<Integer> res){
+        if(node==null) return;
+        res.add(node.val);
+        traverse(node.left, res);
+        traverse(node.right, res);
+    }
+}

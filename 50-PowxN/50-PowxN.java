63f4a4a75c2a442ba6f507040ad156ffabe5ea7e
Monikaselvamgit <166580400+Monikaselvamgit@users.noreply.github.com>
2026-07-09 09:45:05 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.java
@@ -0,0 +1,12 @@
+// Last updated: 7/9/2026, 9:45:05 AM
+import java.util.*;
+class Solution {
+public double myPow(double x, int n) {
+        double result=Math.pow(x,n);
+        // double res=1;
+        // for(int i=0;i<n;i++){
+        //     res*=x;
+        // }
+        return result;
+    }
+};

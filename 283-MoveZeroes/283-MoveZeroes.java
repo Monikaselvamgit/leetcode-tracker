d4ec08816f2b05ccd2fbe479a4e24b5a05875829
Monikaselvamgit <166580400+Monikaselvamgit@users.noreply.github.com>
2026-07-09 09:44:23 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 9:44:23 AM
+class Solution {
+    public void moveZeroes(int[] nums){ 
+        int index=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+               nums[index]=nums[i];
+               index++; 
+            }
+        }
+         while(index<nums.length){
+                nums[index]=0;
+                index++;
+            }
+    }
+}
